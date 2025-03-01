78532398f03049b243988ecb1dfc69d0ad5558b1
Your Name <project.email@example.com>
2025-03-01 06:31:43 +0300

    optimize js code

--- src/main.js
+++ src/main.js
@@ -11,103 +11,106 @@ let toDekameters;
 let toHectometers;
 let toKilometers;
 
-let working = false;
+let noError = false;
+
+
 
 convertButton.addEventListener("click", () => {
   checkError();
 
-  if (working === true) {
-    if (selectElement.value === "millimeters") {
-      toMillimeters = inputElement.value / 1;
-      toCentimeters = inputElement.value / 10;
-      toDecimeters = inputElement.value / 100;
-      toMeters = inputElement.value / 1000;
-      toDekameters = inputElement.value / 10000;
-      toHectometers = inputElement.value / 100000;
-      toKilometers = inputElement.value / 1000000;
-    } else if (selectElement.value === "centimeters") {
-      toMillimeters = inputElement.value * 10;
-      toCentimeters = inputElement.value / 1;
-      toDecimeters = inputElement.value / 10;
-      toMeters = inputElement.value / 100;
-      toDekameters = inputElement.value / 1000;
-      toHectometers = inputElement.value / 10000;
-      toKilometers = inputElement.value / 100000;
-    } else if (selectElement.value === "decimeters") {
-      toMillimeters = inputElement.value * 100;
-      toCentimeters = inputElement.value * 10;
-      toDecimeters = inputElement.value / 1;
-      toMeters = inputElement.value / 10;
-      toDekameters = inputElement.value / 100;
-      toHectometers = inputElement.value / 1000;
-      toKilometers = inputElement.value / 10000;
-    } else if (selectElement.value === "meters") {
-      toMillimeters = inputElement.value * 1000;
-      toCentimeters = inputElement.value * 100;
-      toDecimeters = inputElement.value * 10;
-      toMeters = inputElement.value / 1;
-      toDekameters = inputElement.value / 10;
-      toHectometers = inputElement.value / 100;
-      toKilometers = inputElement.value / 1000;
-    } else if (selectElement.value === "dekameter") {
-      toMillimeters = inputElement.value * 10000;
-      toCentimeters = inputElement.value * 1000;
-      toDecimeters = inputElement.value * 100;
-      toMeters = inputElement.value * 10;
-      toDekameters = inputElement.value / 1;
-      toHectometers = inputElement.value / 10;
-      toKilometers = inputElement.value / 100;
-    } else if (selectElement.value === "hectometer") {
-      toMillimeters = inputElement.value * 100000;
-      toCentimeters = inputElement.value * 10000;
-      toDecimeters = inputElement.value * 1000;
-      toMeters = inputElement.value * 100;
-      toDekameters = inputElement.value * 10;
-      toHectometers = inputElement.value / 1;
-      toKilometers = inputElement.value / 10;
-    } else if (selectElement.value === "kilometer") {
-      toMillimeters = inputElement.value * 1000000;
-      toCentimeters = inputElement.value * 100000;
-      toDecimeters = inputElement.value * 10000;
-      toMeters = inputElement.value * 1000;
-      toDekameters = inputElement.value * 100;
-      toHectometers = inputElement.value * 10;
-      toKilometers = inputElement.value / 1;
-    } 
-
-
+  if (noError === true) {
+    doConversion();
     displayResults();
   }
-
-
 });
+
+
 
 function checkError() {
   if (inputElement.value !== "" && selectElement.value !== "base unit") {
-    working = true;
+    noError = true;
     inputElement.classList.remove("red-border");
     selectElement.classList.remove("red-border");
     errorText.textContent = "";
   } else if (inputElement.value === "" && selectElement.value === "base unit") {
-    working = false;
+    noError = false;
     inputElement.classList.add("red-border");
     selectElement.classList.add("red-border");
     errorText.textContent = "Please input a value and select a base unit";
   } else if (inputElement.value === "" && selectElement.value !== "base unit") {
-    working = false;
+    noError = false;
     inputElement.classList.add("red-border");
     selectElement.classList.remove("red-border");
     errorText.textContent = "Please input a value";
   } else if (inputElement.value !== "" && selectElement.value === "base unit") {
-    working = false;
+    noError = false;
     inputElement.classList.remove("red-border");
     selectElement.classList.add("red-border");
     errorText.textContent = "Please select a base unit";
   }
 }
 
+function doConversion() {
+  if (selectElement.value === "millimeters") {
+    toMillimeters = inputElement.value / 1;
+    toCentimeters = inputElement.value / 10;
+    toDecimeters = inputElement.value / 100;
+    toMeters = inputElement.value / 1000;
+    toDekameters = inputElement.value / 10000;
+    toHectometers = inputElement.value / 100000;
+    toKilometers = inputElement.value / 1000000;
+  } else if (selectElement.value === "centimeters") {
+    toMillimeters = inputElement.value * 10;
+    toCentimeters = inputElement.value / 1;
+    toDecimeters = inputElement.value / 10;
+    toMeters = inputElement.value / 100;
+    toDekameters = inputElement.value / 1000;
+    toHectometers = inputElement.value / 10000;
+    toKilometers = inputElement.value / 100000;
+  } else if (selectElement.value === "decimeters") {
+    toMillimeters = inputElement.value * 100;
+    toCentimeters = inputElement.value * 10;
+    toDecimeters = inputElement.value / 1;
+    toMeters = inputElement.value / 10;
+    toDekameters = inputElement.value / 100;
+    toHectometers = inputElement.value / 1000;
+    toKilometers = inputElement.value / 10000;
+  } else if (selectElement.value === "meters") {
+    toMillimeters = inputElement.value * 1000;
+    toCentimeters = inputElement.value * 100;
+    toDecimeters = inputElement.value * 10;
+    toMeters = inputElement.value / 1;
+    toDekameters = inputElement.value / 10;
+    toHectometers = inputElement.value / 100;
+    toKilometers = inputElement.value / 1000;
+  } else if (selectElement.value === "dekameter") {
+    toMillimeters = inputElement.value * 10000;
+    toCentimeters = inputElement.value * 1000;
+    toDecimeters = inputElement.value * 100;
+    toMeters = inputElement.value * 10;
+    toDekameters = inputElement.value / 1;
+    toHectometers = inputElement.value / 10;
+    toKilometers = inputElement.value / 100;
+  } else if (selectElement.value === "hectometer") {
+    toMillimeters = inputElement.value * 100000;
+    toCentimeters = inputElement.value * 10000;
+    toDecimeters = inputElement.value * 1000;
+    toMeters = inputElement.value * 100;
+    toDekameters = inputElement.value * 10;
+    toHectometers = inputElement.value / 1;
+    toKilometers = inputElement.value / 10;
+  } else if (selectElement.value === "kilometer") {
+    toMillimeters = inputElement.value * 1000000;
+    toCentimeters = inputElement.value * 100000;
+    toDecimeters = inputElement.value * 10000;
+    toMeters = inputElement.value * 1000;
+    toDekameters = inputElement.value * 100;
+    toHectometers = inputElement.value * 10;
+    toKilometers = inputElement.value / 1;
+  } 
+};
+
 function displayResults() {
-  if(working === true) {
   let abbreviation;
 
   if (selectElement.value === "millimeters") {
@@ -155,5 +158,5 @@ function displayResults() {
         <span>${inputElement.value} ${abbreviation}</span> is equal to <span>${toKilometers} km</span>
       </p>
   `;
-}
+
 }
